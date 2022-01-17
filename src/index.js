a97cc1a821b523208cdf61796511dac250ad2da4
prathamn007 <pratham.191mt034@nitk.edu.in>
2022-01-17 18:46:11 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import { ResultContextProvider } from './contexts/ResultContextProvider';
+import './input.css'
+
+ReactDom.render(
+    <ResultContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ResultContextProvider>, document.getElementById('root'),
+);
